3b48f67470be4850ddc00c3aa4cca962565d1aab
Adal <igiranezaadal@gmail.com>
2026-09-09 16:30:03 +0200

    datatypes in TS

--- Typescript/src/index.ts
+++ Typescript/src/index.ts
@@ -4,14 +4,32 @@ console.log('hello Adal');
 
 
 // codewars
- function noSpace(x:string):string {
-    // let result = x.replace(/\s/g, '');
-    let result = '';
-    for (let i = 0; i < x.length; i++) {
-        if (x[i] !== ' ') {
-            result += x[i];
-        }
-    }
-    return result;
+//  function noSpace(x:string):string {
+//     // let result = x.replace(/\s/g, '');
+//     let result = '';
+//     for (let i = 0; i < x.length; i++) {
+//         if (x[i] !== ' ') {
+//             result += x[i];
+//         }
+//     }
+//     return result;
+// }
+// console.log(noSpace("hell Adal"));
+
+
+
+function descendingOrder(n: number|string): number {
+  // your code here
+  
+//   let toText = String(n)
+//   toText = toText.reverse('')
+//   return Number(toText)
+
+    // n = String(n)
+    // return n.split('').sort((a,b)=>b-a).join('')
+
+  return Number(String(n).split('')
+                  .sort((a,b)=>Number(b) - Number(a))
+                  .join(''));
 }
-console.log(noSpace("hell Adal"));
+console.log(descendingOrder(12345));
