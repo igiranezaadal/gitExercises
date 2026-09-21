2f5b412a527d45c6d1c1be578deb645fece21a2c
Adal <igiranezaadal@gmail.com>
2026-09-21 16:57:52 +0200

    conditions

---
+++ Typescript/controlFlow.ts
@@ -0,0 +1,9 @@
+var n: number=5
+while(n>5){
+    console.log("enter while");
+}
+
+do{
+    console.log(`entered dowhile`);  
+}
+while(n>5)
---
+++ Typescript/ex.ts
@@ -0,0 +1,142 @@
+// export interface User  {
+//     name: string,
+//     age: number,
+//     occupation: string
+// };
+
+// export const users: User[] = [
+//     {
+//         name: 'Max Mustermann',
+//         age: 25,
+//         occupation: 'Chimney sweep'
+//     },
+//     {
+//         name: 'Kate Mülzler',
+//         age: 23,
+//         occupation: 'Astronaut'
+//     }
+// ];
+
+// export function logPerson(user: User) {
+//     console.log(` - ${user.name}, ${user.age}`);
+// }
+
+// console.log('Users:');
+// users.forEach(logPerson);
+
+
+
+
+
+
+// // ex 2
+// interface User {
+//     name: string;
+//     age: number;
+//     occupation: string;
+// }
+
+// interface Admin {
+//     name: string;
+//     age: number;
+//     role: string;
+// }
+
+// export type Person = User| Admin;
+
+// export const persons: Person[] /* <- Person[] */ = [
+//     {
+//         name: 'Max Mustermann',
+//         age: 25,
+//         occupation: 'Chimney sweep'
+//     },
+//     {
+//         name: 'Jane Doe',
+//         age: 32,
+//         role: 'Administrator'
+//     },
+//     {
+//         name: 'Kate Müller',
+//         age: 23,
+//         occupation: 'Astronaut'
+//     },
+//     {
+//         name: 'Bruce Willis',
+//         age: 64,
+//         role: 'World saver'
+//     }
+// ];
+
+// export function logPerson(user: Person) {
+//     console.log(` - ${user.name}, ${user.age}`);
+// }
+
+// persons.forEach(logPerson);
+
+
+
+// // ex 3
+// Intro:
+
+//     Since we already have some of the additional
+//     information about our users, it's a good idea
+//     to output it in a nice way.
+
+// Exercise:
+
+//     Fix type errors in logPerson function.
+
+//     logPerson function should accept both User and Admin
+//     and should output relevant information according to
+//     the input: occupation for User and role for Admin.
+
+// */
+
+interface User {
+    name: string;
+    age: number;
+    occupation: string;
+}
+
+interface Admin {
+    name: string;
+    age: number;
+    role: string;
+}
+
+export type Person = User | Admin;
+
+export const persons: Person[] = [
+    {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep'
+    },
+    {
+        name: 'Jane Doe',
+        age: 32,
+        role: 'Administrator'
+    },
+    {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut'
+    },
+    {
+        name: 'Bruce Willis',
+        age: 64,
+        role: 'World saver'
+    }
+];
+
+export function logPerson(person: User | Admin) {
+    let additionalInformation: string;
+    if (person.role) {
+        additionalInformation = person.role;
+    } else {
+        additionalInformation = person.occupation;
+    }
+    console.log(` - ${person.name}, ${person.age}, ${additionalInformation}`);
+}
+
+persons.forEach(logPerson);
