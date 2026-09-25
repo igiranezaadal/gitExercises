e2f79ca287561fce274d74eda9523442fe2bb3e1
Adal <igiranezaadal@gmail.com>
2026-09-25 16:51:39 +0200

    acces modifiers in class private

--- Typescript/join.ts
+++ Typescript/join.ts
@@ -1,1 +1,23 @@
 
+// Public class Person {
+//   name: string;
+// }
+
+// const person = new Person();
+// person.name = "Jane";
+
+class Person {
+  private name: string;
+
+  public constructor(name: string) {
+    this.name = name;
+  }
+
+  public getName(): string {
+    return this.name;
+  }
+}
+
+const person = new Person("Jane");
+console.log(person.getName()); // no error
+// console.log(person.name) // cant accsess private accesss modifireer
